040526031bb0bb43d12c1c69e77465ef21bab463
3j <nu3jane@gmail.com>
2012-01-05 14:51:54 +0100

    Added a Users controller and layout link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector("title", :content => "Home")
@@ -20,4 +21,23 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector("title", :content => "Help")
   end
+
+  it "should have a SignUp page at '/signup'" do
+    get '/signup'
+    response.should have_selector("title", :content => "Sign up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector "title", :content => "Home"
+    click_link 'About'
+    response.should have_selector "title", :content => "About"
+    click_link 'Contact'
+    response.should have_selector "title", :content => "Contact"
+    click_link 'Home'
+    response.should have_selector 'title', :content => 'Home'
+    click_link 'Sign up now!'
+    response.should have_selector 'title', :content => 'Sign up'
+  end
+
 end
